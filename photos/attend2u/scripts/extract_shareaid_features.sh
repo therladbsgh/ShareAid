24ed09ba4859f00b99410c7c196602de15f0aa42
Philip Hinch <pjhinch@gmail.com>
2018-05-15 01:20:14 -0400

    deleting attend2u

--- photos/attend2u/scripts/extract_shareaid_features.sh
+++
@@ -1,4 +0,0 @@
-python generate_shareaid_dataset.py
-python cnn_feature_extractor.py --gpu_id 0 --batch_size 32 --input_fname ../data/caption_dataset/train.txt
-python cnn_feature_extractor.py --gpu_id 0 --batch_size 32 --input_fname ../data/caption_dataset/test.txt
-
